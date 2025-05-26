e89c4b1a8b906891857b6ff6f6f1f98766875808
xyryc <amdanikzz@gmail.com>
2025-05-26 14:46:09 +0600

    implemented get all books api

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -34,4 +34,30 @@ router.post("/", protectRoute, async (req, res) => {
   }
 });
 
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 }) //descending
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error in get all book route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default router;
