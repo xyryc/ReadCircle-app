8ee7bebb341011bcc154baf6702e6dcf739dbda4
Md Anik <amdanikzz@gmail.com>
2025-06-20 01:27:20 +0600

    updated cron job

--- backend/src/lib/cron.js
+++ backend/src/lib/cron.js
@@ -1,9 +1,11 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+// const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new cron.CronJob("* * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
+      console.log(process.env.API_URL);
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Database connected ${conn.connection.host}`);
+    console.log("Connected to:", conn.connection.name);
   } catch (error) {
     console.log("Error connecting to database");
     process.exit(1);
